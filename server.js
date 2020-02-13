9ee447b671166013cc64990f80f18a6190a150bc
Larry Gonzalez <larry.g.m1024@gmail.com>
2020-02-13 07:28:56 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+// require ('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+// express hbs
+hbs.registerPartials(__dirname + '/views/parcials');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) =>{
+
+  res.render('home',{
+    nombre:'larry González'});
+});
+  app.get('/about', (req, res) =>{
+
+    res.render('about',{
+      heroe:'Batman'});
+  
+});
+
+app.listen(port,()=>{
+    console.log(`El puerto ${ port } se esta escuchando`)
+});
